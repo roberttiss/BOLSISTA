6dc09cbb99c589ba81f9fe8a9da184fe2c4aa1c3
gabriel roberts <gabrielrobertssoares@gmail.com>
2023-07-05 16:53:48 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace faculdade
+{
+    class Aluno
+    {
+        //atributos da classe
+        public String nome;
+        public String rm;
+        public Boolean ehBolsista;
+        public double mensalidadefinal;
+
+        //método para checar se é bolsista
+        public void calculaMensalidade()
+        {
+            if (this.ehBolsista == true)
+            {
+                this.mensalidadefinal = 200.00;//é bolsista 50% de desconto
+            }
+            else
+            {
+                this.mensalidadefinal = 400.00;//não é bolsista então valor cheio
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace faculdade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //instancia da classe
+        Aluno reg = new Aluno();//dizendo para a memória Ram que faremos uso da classe Aluno
+        private void calculaMensalidade_Click(object sender, EventArgs e)
+        {
+            //entrada de dados para os atributos
+            reg.nome = txtNOME.Text;
+            reg.rm = txtRM.Text;
+
+
+
+            if (checkBox1.Checked == true)//é bolsista ou seja o checkbox está clicado
+            {
+                reg.ehBolsista = true;//manda true para atributo ehBolsista
+
+
+
+                reg.calculaMensalidade();//chama o método da classe
+
+
+
+                //mostraRegistro ou seja mostra todos os dados da classe
+                MessageBox.Show("Nome do aluno: " + reg.nome + "\nMensalidade: " + reg.mensalidadefinal);
+
+            }
+            else//não é bolsista
+            {
+                reg.ehBolsista = false;//manda true para atributo ehBolsista
+
+
+
+                reg.calculaMensalidade();//chama o método da classe
+
+
+
+                //mostraRegistro ou seja mostra todos os dados da classe
+                MessageBox.Show("Nome do aluno: " + reg.nome + "\nMensalidade: " + reg.mensalidadefinal);
+            }
+        }
+    }
+}
